5dc3a65bc104fb08889e6b78dc44144673a946c0
Divya K R <k.raj.divya@gmail.com>
2019-12-24 16:10:44 +0530

    class details

---
+++ CRUD/index.js
@@ -0,0 +1,22 @@
+const contacts = []
+document.getElementById("form1").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const mobile = document.getElementById("mobile").value;
+    const contact = {
+        name: name,
+        email: email,
+        mobile: mobile
+    }
+    contacts.push(contact)
+    let displayText = "";
+    contacts.forEach(contact => {
+        displayText = displayText + `<div style= "border: 1px solid black; margin: 10px; padding:10px;>
+                                        <h1>${contact.name}</h1>
+                                        <h1>${contact.email}</h1>
+                                        <h1>${contact.mobile}</h1>
+                                    </div>`
+        document.getElementById("displayContacts").innerHTML = displayText;    
+    })
+})
